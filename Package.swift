b02e982e1644884f7233108f0eaead1010bdb1b2
Wang, Michael <ch_w@live.com>
2016-12-16 17:51:27 +0800

    Update Post model

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "vapor-example",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2)
     ],
     exclude: [
         "Config",
--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -4,33 +4,31 @@ import Foundation
 
 final class Post: Model {
     var id: Node?
+    var title: String
     var content: String
+    var author: String
     
-    init(content: String) {
+    init(title: String, content: String, author: String) {
         self.id = UUID().uuidString.makeNode()
+        self.title = title
         self.content = content
+        self.author = author
     }
 
     init(node: Node, in context: Context) throws {
         id = try node.extract("id")
+        title = try node.extract("title")
         content = try node.extract("content")
+        author = try node.extract("author")
     }
 
     func makeNode(context: Context) throws -> Node {
         return try Node(node: [
             "id": id,
+            "title": title,
+            "author": author,
             "content": content
         ])
-    }
-}
-
-extension Post {
-    /**
-        This will automatically fetch from database, using example here to load
-        automatically for example. Remove on real models.
-    */
-    public convenience init?(from string: String) throws {
-        self.init(content: string)
     }
 }
 
